5b6a0b2298a22367ccc2b179adf319da37af9903
kishangabani <kishan.gabani600@gmail.com>
2018-12-12 11:44:02 -0500

    object create

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ let b=undefined;
 let c=true;
 let d=null;
 console.log(a,b, c, d);
+
+//object
+let person={
+name:'KIshan',
+age: 24
+};
+//dot notation
+person.name='KI';
+console.log(person.name);
+
+//bracket notation
+person['age']=25;
+console.log(person.age);
